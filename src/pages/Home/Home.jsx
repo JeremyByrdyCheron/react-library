f882220e520f9feca3a6df5db6e556a80368d98f
Jeremybyc <jbc60510@outlook.fr>
2026-02-23 09:35:33 +0100

    functionning like button

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,5 +1,82 @@
+import Book from "../../components/Book/Book";
+
 const Home = () => {
-  return <h1>Titlz</h1>;
+  const livresInitiaux = [
+    {
+      id: 1,
+      nom: "Le Seigneur des Anneaux",
+      description: "Une épopée fantastique en Terre du Milieu",
+      genre: "Fantasy",
+      like: 150,
+      favorite: true,
+    },
+    {
+      id: 2,
+      nom: "1984",
+      description: "Un roman dystopique sur la surveillance de masse",
+      genre: "Science-Fiction",
+      like: 89,
+      favorite: true,
+    },
+    {
+      id: 3,
+      nom: "Orgueil et Préjugés",
+      description: "Une histoire d'amour dans l'Angleterre du 19e siècle",
+      genre: "Romance",
+      like: 67,
+      favorite: false,
+    },
+    {
+      id: 4,
+      nom: "Dune",
+      description: "Une saga de science-fiction sur la planète Arrakis",
+      genre: "Science-Fiction",
+      like: 120,
+      favorite: false,
+    },
+    {
+      id: 5,
+      nom: "Harry Potter",
+      description: "Les aventures d'un jeune sorcier",
+      genre: "Fantasy",
+      like: 200,
+      favorite: false,
+    },
+    {
+      id: 6,
+      nom: "Sherlock Holmes",
+      description: "Les enquêtes du célèbre détective",
+      genre: "Policier",
+      like: 95,
+      favorite: false,
+    },
+    {
+      id: 7,
+      nom: "Le Petit Prince",
+      description: "Un conte poétique et philosophique",
+      genre: "Conte",
+      like: 180,
+      favorite: false,
+    },
+    {
+      id: 8,
+      nom: "Fondation",
+      description: "Le déclin d'un empire galactique",
+      genre: "Science-Fiction",
+      like: 75,
+      favorite: true,
+    },
+  ];
+  return livresInitiaux.map((book) => (
+    <Book
+      id={book["id"]}
+      name={book["nom"]}
+      description={book["description"]}
+      genre={book["genre"]}
+      like={book["like"]}
+      favorite={book["favorite"]}
+    />
+  ));
 };
 
 export default Home;
